e31f5aa6d1e8c11b4789c30addfadfc4a65300cf
Tiphanie DEPREAUX <tiphanie.depreaux@ens.uvsq.fr>
2022-04-12 11:07:47 +0200

    ajout de l'en tête

---
+++ fourmi_de_langton.py
@@ -0,0 +1,6 @@
+#################################
+# Groupe MI TD2 (groupe de projet n°3)
+# Tiphanie DEPREAUX
+# Baptiste PARIS
+# Ania AOUAOUCHE
+# https://github.com/uvsq22102500/Fourmi-de-Langton
